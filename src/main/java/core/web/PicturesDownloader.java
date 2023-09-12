29dd3f72cd5824b7f74a0f3e310e70e1327a7c26
Gašper Pintar <pintargaper1@gmail.com>
2023-09-12 20:54:24 +0200

    update - 12.09.2023

--- src/main/java/core/web/PicturesDownloader.java
+++ src/main/java/core/web/PicturesDownloader.java
@@ -1,7 +1,6 @@
 package core.web;
 
 import core.database.API;
-
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.net.URL;
@@ -12,7 +11,7 @@ import java.nio.file.StandardCopyOption;
 public class PicturesDownloader {
 
     public static final String PROFILE_PICTURE = API.HOST + "profile-picture";
-    private static final String gameNameFolder = "MemoStickRescue";
+    private static final String gameNameFolder = "Memo Stick Rescue";
     private static final String picturesFolder = "pictures";
     private static final String profilePicturesFolder = "profile";
     private static final String basicPath = FileSystemView.getFileSystemView().getDefaultDirectory() + "/" + gameNameFolder + "/" + picturesFolder + "/" + profilePicturesFolder + "/";
